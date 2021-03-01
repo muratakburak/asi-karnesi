ec4428f43c034e532ae13e975fe6560da159387c
elifbasak <yildirimelifbasak@gmail.com>
2021-03-02 01:09:45 +0300

    Initial Commit

---
+++ Google/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Google
+//
+//  Created by Elif Basak  Yildirim on 2.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GoogleTests/GoogleTests.swift
@@ -0,0 +1,33 @@
+//
+//  GoogleTests.swift
+//  GoogleTests
+//
+//  Created by Elif Basak  Yildirim on 2.03.2021.
+//
+
+import XCTest
+@testable import Google
+
+class GoogleTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
