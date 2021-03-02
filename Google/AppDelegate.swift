32ab0cdb234b82b6961fd9d3aa8e9749496bb641
elifbasak <yildirimelifbasak@gmail.com>
2021-03-02 03:21:27 +0300

    Sign In with Google

--- Google/AppDelegate.swift
+++ Google/AppDelegate.swift
@@ -4,19 +4,35 @@
 //
 //  Created by Elif Basak  Yildirim on 2.03.2021.
 //
-
+import GoogleSignIn
+import Firebase
 import UIKit
 
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, GIDSignInDelegate {
 
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        FirebaseApp.configure()
+        GIDSignIn.sharedInstance()?.clientID="878560007334-gc168kahsmkmn0j06d9ltfnbnlhmu8c4.apps.googleusercontent.com"
+        GIDSignIn.sharedInstance()?.delegate=self
         return true
     }
-
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        let userId = user.userID
+        let idToken = user.authentication.idToken // Safe to send to the server
+          let fullName = user.profile.name
+          let givenName = user.profile.givenName
+          let familyName = user.profile.familyName
+          let email = user.profile.email
+        print("User email: \(user.profile.email ?? "No Email")")
+        print("User tokenId: \(user.authentication.idToken ?? "No Email")")
+    }
+    func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        return GIDSignIn.sharedInstance().handle(url)
+    }
     // MARK: UISceneSession Lifecycle
 
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
--- Google/ViewController.swift
+++ Google/ViewController.swift
@@ -6,12 +6,12 @@
 //
 
 import UIKit
-
+import GoogleSignIn
 class ViewController: UIViewController {
-
+    @IBOutlet var signInButton: GIDSignInButton!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        GIDSignIn.sharedInstance().presentingViewController=self
     }
 
 
